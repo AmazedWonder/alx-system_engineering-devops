4406bc499e60620a8f975073dd50c51b4f276587
AmazedWonder <giftedwonda@gmail.com>
2023-10-28 15:10:45 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
 #!/usr/bin/pup
 # Using Puppet, install flask Version 2.1.0 from pip3.
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#package { 'flask':
+  #ensure   => '2.1.0',
+  #provider => 'pip3',
+#}
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin',
+  refreshonly => true,
+}
+
+# Optional: Notify a service restart or other resource that depends on Flask
+# service { 'my_app':
+#   ensure  => running,
+#   require => Exec['install_flask'],
+# }
