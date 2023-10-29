1b595a7cbabe408a92f0499e198bbc116b40b38a
AmazedWonder <giftedwonda@gmail.com>
2023-10-29 12:20:12 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,15 +6,15 @@ file { '/etc/ssh/sshd_config':
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/sshd_config',
-  line    => 'PasswordAuthentication no',
-  match   => '#PasswordAuthentication',
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path    => '/etc/ssh/sshd_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '#IdentityFile',
+  path  => '/etc/ssh/sshd_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '#IdentityFile',
 }
 #file { '/etcssh/config':
   #ensure  => present,
