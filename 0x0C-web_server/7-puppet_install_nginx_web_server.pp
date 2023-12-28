fb0440829650c336bc480b55d7c72a81ee39337c
AmazedWonder <giftedwonda@gmail.com>
2023-12-28 04:11:14 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,39 +2,44 @@
 # Configure server using puppet
 
 # defines a Puppet class called nginx_server that 
-#  encapsulates the configuration for the Nginx server.
-class nginx_server {
-  package { 'nginx':
-    ensure => installed,
-  }
+# encapsulates the configuration for the Nginx server.
+# Script to install nginx using puppet
 
-#  manages the Nginx service.
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-  }
-# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "
-      server {
-        listen      80 default_server;
-        listen      [::]:80 default_server;
-        root        /var/www/html;
-        index       index.html index.htm;
-
-        location / {
-          return 200 'Hello World!';
-        }
-
-        location /redirect_me {
-          return 301 http://cuberule.com/;
-        }
-      }
-    ",
-    notify => Service['nginx'],
-  }
+package { 'nginx':
+  ensure => 'present',
 }
-#  includes the nginx_server class, ensuring that it gets applied.
-include nginx_server
+
+exec { 'install_nginx':
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
+  path     => '/usr/bin',
+  creates  => '/etc/nginx',
+  require  => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Exec['install_nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      root /var/www/html;
+      index index.html;
+
+      location /redirect_me {
+        return 301 https://blog.ehoneahobed.com/;
+      }
+    }
+  ',
+  require => File['/var/www/html/index.html'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
