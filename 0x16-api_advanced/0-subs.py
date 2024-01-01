16c20055f9cf841263473fbbe8d000ebded3dbec
AmazedWonder <giftedwonda@gmail.com>
2024-01-01 05:17:16 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Ubuntu:0x16-api_advanced:v1.0.0 \
-        (by /u/Comfortable-Love3395)"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
