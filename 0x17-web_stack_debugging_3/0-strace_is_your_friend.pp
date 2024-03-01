2821e9bb79f4c59b9f201ff7014e59ede73222d2
AmazedWonder <giftedwonda@gmail.com>
2024-03-01 11:49:43 +0100

    fix apache returning error 500 with puppet file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# this fixes error file ext "phpp" to "php" correction
+
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
