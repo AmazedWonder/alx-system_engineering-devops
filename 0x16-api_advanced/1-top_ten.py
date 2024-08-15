b773d1ea1accb0161b948dc267292ff93b3a1d8d
AmazedWonder <giftedwonda@gmail.com>
2024-08-15 05:30:31 +0100

    update 1-top_ten file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,8 @@ def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Comfortable-Love3395)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0 \
+                    (by /u/Comfortable-Love3395)"
     }
     params = {
         "limit": 10
